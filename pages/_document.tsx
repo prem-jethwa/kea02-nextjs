1363741b1c5dc9657e1907924b7ee060bb1f5ef3
prem-jethwa <premjethwa788@gmail.com>
2022-06-12 19:08:46 +0530

    added _document, favicon and file cleanup

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Prem Jethwa</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
